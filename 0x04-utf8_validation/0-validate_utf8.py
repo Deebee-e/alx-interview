b18c3652306095c6f3671d0a35dec141ad3f5d28
Deebee-e <elideb4life@gmail.com>
2024-11-04 05:29:41 +0100

    utf ncoding

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,28 +1,38 @@
 #!/usr/bin/python3
+"""
+Determines if a given data set represents a valid UTF-8 encoding
+"""
+
 
 def validUTF8(data):
-    num_bytes = 0  # Number of bytes expected in the current character
+    """
+    Validates UTF-8 encoding
 
-    for num in data:
-        # Mask the number to get the last 8 bits
-        byte = num & 0xFF
-        # Determine the number of bytes expected
-        if num_bytes == 0:
-            if (byte >> 7) == 0b0:  # 0xxxxxxx
-                num_bytes = 0
-            elif (byte >> 5) == 0b110:  # 110xxxxx
-                num_bytes = 1
-            elif (byte >> 4) == 0b1110:  # 1110xxxx
-                num_bytes = 2
-            elif (byte >> 3) == 0b11110:  # 11110xxx
-                num_bytes = 3
+    Args:
+        data (list): A dataset represented by a list of integers
+
+    Return:
+        True if data is a valid UTF-8 encoding, else False
+    """
+
+    byte_count = 0
+
+    for byte in data:
+        if byte_count == 0:
+            if (byte >> 7) == 0b0:
+                # Single byte character, no need for further checks
+                continue
+            elif (byte >> 5) == 0b110:
+                byte_count = 1
+            elif (byte >> 4) == 0b1110:
+                byte_count = 2
+            elif (byte >> 3) == 0b11110:
+                byte_count = 3
             else:
-                return False  # Invalid starting byte
+                return False
         else:
-            # Check continuation byte (10xxxxxx)
             if (byte >> 6) != 0b10:
-                return False  # Invalid continuation byte
-            num_bytes -= 1
+                return False
+            byte_count -= 1
 
-    # If we are still expecting bytes after processing all data, it's invalid
-    return num_bytes == 0
+    return byte_count == 0
